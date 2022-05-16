9f7a63c0e606570c8bbb4a3a46ec58541d3652e5
Nicki316 <nicki831028@gmail.com>
2022-05-16 14:44:24 +0800

    3.4批次爬取子網頁

--- scrapter.py
+++ scrapter.py
@@ -1,7 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
+import urllib3
+urllib3.disable_warnings()
 
-res = requests.get('https://www.bbc.com/zhongwen/trad/topics/c32p4kj2yzqt')
+res = requests.get('https://www.bbc.com/zhongwen/trad/topics/c32p4kj2yzqt', verify=False)
 #print(res.text.find())
 
 soup = BeautifulSoup(res.text,'lxml')
@@ -13,4 +15,12 @@ titlelist = []
 for title in titles:
     titlelist.append(title.getText())
     
-print (titlelist)
+tag_list = []
+urls = soup.find_all('a', {'class': 'qa-heading-link lx-stream-post__header-link'})
+for url in urls:
+    sub_response = requests.get('https://www.bbb.com'+ url.get('href'), verify = False)
+    sub_soup = BeautifulSoup(sub_response.text,'lxml')
+    tags = sub_soup.find_all('li',{'class':'bbc-1msyfg1 e1hq59l0'})
+    for tag in tags:
+        tag_list.append(tag.getText())
+        print (tag.getText())
